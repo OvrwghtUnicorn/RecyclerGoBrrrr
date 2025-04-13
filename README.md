d6fe3582c6c110859bf3a90f9c56c823aba5cf7e
OvrwghtUnicorn <overweightunicorn.studios@gmail.com>
2025-04-12 18:54:29 -0700

    Version 1.0.0 Release

---
+++ README.md
@@ -0,0 +1,13 @@
+# Recycler Go Brrrr
+Print money using the 'Cash for Trash' Recyclers by simply opening the lid and pressing a button. Why pay $100 a day to hire a cleaner when you can earn $300 a day from the trash the game naturally spawns.
+
+This mod is only for the Main branch of the game. Have not tested it on any of the Beta Branches.
+
+# Summary
+Are you tired of picking up individual garbage items and placing them in the trash? Do you really feel like its worth spending $100 a day on a cleaner for your property? This mod makes it easier to get money for collecting trash. When you lift the handle the recycler will check to see if there is any trash in the world. If there is you can press the button to get your payout.
+
+
+# Installation
+1. Download and install [MelonLoader](https://github.com/LavaGang/MelonLoader/releases)
+1. Download this mod.
+1. Unzip the downloaded file and drag the .dll into the "Mods" folder in your Schedule I files.
---
+++ RecyclerGoBrrrr/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+[assembly: MelonInfo(typeof(RecyclerGoBrrrr), "Recycler Go Brrrr", "1.0.0", "OverweightUnicorn")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+[assembly: MelonColor(255, 252, 0, 119)]
+[assembly: MelonAuthorColor(255, 194, 0, 255)]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RecyclerGoBrrrr")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("UnicornsCanMod")]
+[assembly: AssemblyProduct("RecyclerGoBrrrr")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("98da1795-4fff-487e-99f3-da060c742459")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
