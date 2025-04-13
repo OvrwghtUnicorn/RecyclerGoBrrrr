a5443a7c0da87688705d0ab25e378ad2035e2bed
delizoderek <dwdelizo@gmail.com>
2025-04-12 18:54:31 -0700

    Add project files.

---
+++ RecyclerGoBrrrr/RecyclerGoBrrrr.cs
@@ -0,0 +1,28 @@
+﻿using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+using Il2CppScheduleOne.Trash;
+using Il2CppScheduleOne.ObjectScripts;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using Il2CppScheduleOne.Interaction;
+
+
+public class RecyclerGoBrrrr : MelonMod {
+
+    [HarmonyPatch(typeof(Recycler), nameof(Recycler.GetTrash))]
+    static class RecyclerGetTrashPatch {
+        static bool Prefix(ref Il2CppReferenceArray<TrashItem> __result) {
+            TrashItem[] trashList = GameObject.FindObjectsOfType<TrashItem>();
+            __result = new Il2CppReferenceArray<TrashItem>(trashList.Length);
+            for (int i = 0; i < trashList.Length; i++) {
+                __result[i] = trashList[i];
+            }
+
+            return false;
+        }
+    }
+
+    public override void OnInitializeMelon() {
+        MelonLogger.Msg("Converting Recycler to Money Printer");
+    }
+}
